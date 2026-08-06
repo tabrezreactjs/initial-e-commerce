7a30e3308510aef8936b7cb3cf7f58610c5b3abc
tansari21.08 <tansari@gammastack.com>
2026-08-06 18:42:14 +0530

    Initial project setup

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-slate-100">
+      <h1 className="text-5xl font-bold text-blue-600">
+        Tailwind CSS v4 Working 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
